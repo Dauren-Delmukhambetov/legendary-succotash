f5c6ad945d43061750b59058bc4b4125ae0d326b
COCONUTmkII <coconutmkii@gmail.com>
2021-02-25 11:07:55 +0300

    Changed package name from techart to itechart

--- src/main/java/by/itechart/api/UserApiApplication.java
+++ src/main/java/by/itechart/api/UserApiApplication.java
@@ -1,4 +1,4 @@
-package by.techart.api;
+package by.itechart.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/by/itechart/api/UserApiApplicationTests.java
+++ src/test/java/by/itechart/api/UserApiApplicationTests.java
@@ -1,4 +1,4 @@
-package by.techart.api;
+package by.itechart.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
