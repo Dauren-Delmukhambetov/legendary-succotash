f5235c4dcb551d5f36bb086e206449fb6858fc90
COCONUTmkII <coconutmkii@gmail.com>
2021-04-01 17:54:54 +0300

    Created basic exception handler

---
+++ src/main/java/by/itechart/api/exception/UserResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package by.itechart.api.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class UserResponseEntityExceptionHandler extends
+        ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value =
+            {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This is exception test response";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+}
